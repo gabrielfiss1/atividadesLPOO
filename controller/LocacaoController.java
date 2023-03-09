6b011e08fde1b937864e9184dec979064f842e3d
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-03-09 08:46:11 -0300

    Atualizando

--- controller/LocacaoController.java
+++ controller/LocacaoController.java
@@ -6,5 +6,6 @@ public class LocacaoController {
     public static void main(String[] args) {
         Locacao c1 = new Locacao(13/2/2002,("4:32"), 19/2/2002, "18:20", 300000, 400.3, 100.2, true);
         System.out.println(c1);
+        sadasdada
     }
 }
