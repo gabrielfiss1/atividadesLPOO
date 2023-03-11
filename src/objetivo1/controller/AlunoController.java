ad9769ac27156e536374b223415e40f4cd4d137a
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-03-11 20:28:32 -0300

    Novos objetos

--- src/objetivo1/controller/AlunoController.java
+++ src/objetivo1/controller/AlunoController.java
@@ -6,5 +6,11 @@ public class AlunoController {
     public static void main(String[] args) {
         Aluno a1 = new Aluno();
         Aluno a2 = new Aluno();
+        Aluno a3 = new Aluno(1,"Gabriel");
+        Aluno a4 = new Aluno(2, "Jennifer");
+        Aluno a5 = new Aluno(3,999111111, "Carolina", "Silva", "kasilva@gmail.com");
+        Aluno a6 = new Aluno(4,999111112, "Dagobertp", "Suarez", "dagol@gmail.com");
+
+
     }
 }
