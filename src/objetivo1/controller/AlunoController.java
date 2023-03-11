91199e7ac7f9a5dd7d260928f523f6b732b4c4aa
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-03-11 20:23:27 -0300

    Objetos com construtor padrao

--- src/objetivo1/controller/AlunoController.java
+++ src/objetivo1/controller/AlunoController.java
@@ -1,4 +1,10 @@
 package objetivo1.controller;
 
+import objetivo1.model.Aluno;
+
 public class AlunoController {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        Aluno a2 = new Aluno();
+    }
 }
