079e773c2f5f6bfd6076641723a3b7c809e9fc1e
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-03-11 20:13:12 -0300

    Objeto Aluno

--- src/objetivo1/model/Aluno.java
+++ src/objetivo1/model/Aluno.java
@@ -1,4 +1,10 @@
 package objetivo1.model;
 
 public class Aluno {
+    private Integer Id;
+    private Integer cpf;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    
 }
