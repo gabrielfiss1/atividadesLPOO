9df50f9386672f513f9249749810699643181148
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-03-11 20:20:04 -0300

    Adicicionando contrutores e metodos assessores

--- src/objetivo1/model/Aluno.java
+++ src/objetivo1/model/Aluno.java
@@ -6,5 +6,71 @@ public class Aluno {
     private String nome;
     private String sobrenome;
     private String email;
-    
+
+    public Aluno() {
+    }
+
+    public Aluno(Integer id, Integer cpf, String nome, String sobrenome, String email) {
+        Id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+    }
+
+    public Aluno(Integer id, String nome) {
+        Id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "Id=" + Id +
+                ", cpf=" + cpf +
+                ", nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
