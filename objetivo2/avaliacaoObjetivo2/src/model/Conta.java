42dc08dbe40d4f926386e9cb80659cbf136d9a2c
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-04-07 21:47:17 -0300

    feat: adicionando atributos e metodos acessores

---
+++ objetivo2/avaliacaoObjetivo2/src/model/Conta.java
@@ -0,0 +1,24 @@
+package model;
+
+public abstract class Conta {
+    protected Double saldo;
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void deposita(Double valor){
+        this.saldo += valor;
+    }
+
+    public void saca(Double valor){
+        this.saldo -= valor;
+    }
+    public void atualiza(Double taxa){
+        this.saldo = taxa/100 * saldo;
+    }
+}
