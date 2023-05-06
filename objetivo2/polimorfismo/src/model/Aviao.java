0c6e388c6820a6e8d2241cb19aeec8bb0704db3b
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-05-06 16:20:21 -0300

    feat: implementando classes aviao e personagem

--- objetivo2/polimorfismo/src/model/Aviao.java
+++ objetivo2/polimorfismo/src/model/Aviao.java
@@ -1,4 +1,31 @@
 package model;
 
-public class Aviao {
+public class Aviao extends Personagem {
+
+    private double z=0;
+
+    public Aviao(double z) {
+        this.z = z;
+    }
+
+    public Aviao(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Aviao() {
+    }
+
+    public void desenhar() {
+        System.out.println("Desenhou um aviao");
+    }
+
+    @Override
+    public String toString() {
+        return "Aviao{" +
+                "z=" + z +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
