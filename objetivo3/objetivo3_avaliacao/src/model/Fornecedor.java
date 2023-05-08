185196c370ccc5fd0fc65feeb6587a23e52148cb
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-05-07 21:18:56 -0300

    feat: add outras classes

---
+++ objetivo3/objetivo3_avaliacao/src/model/Fornecedor.java
@@ -0,0 +1,13 @@
+package model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fornecedor {
+    private String cnpj;
+    private String contato;
+    private String nome;
+    private List<Produto> produtos = new ArrayList();
+
+}
