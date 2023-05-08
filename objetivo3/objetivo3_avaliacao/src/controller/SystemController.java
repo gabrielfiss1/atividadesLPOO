58d02579b952984b9bae2c02816f393cdd6e9e2b
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-05-07 22:25:53 -0300

    feat: instancias de produtos e itens

--- objetivo3/objetivo3_avaliacao/src/controller/SystemController.java
+++ objetivo3/objetivo3_avaliacao/src/controller/SystemController.java
@@ -7,8 +7,8 @@ import model.Pedido;
 import model.Produto;
 import model.Vendedor;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.text.NumberFormat;
+import java.util.*;
 
 
 public class SystemController {
@@ -17,8 +17,14 @@ public class SystemController {
         Fornecedor fornecedor2 = new Fornecedor("xxxxxxx", "5399994", "Rodrigo Eletro");
         Vendedor vendedor1 = new Vendedor(1, "José", "Avenida Logo Ali", "Santa", "96323333", "Gado Grande", "RS", "011");
         Vendedor vendedor2 = new Vendedor(2, "Hugo", "Rua das Laranjas", "Cidade Baixa", "96321111", "Taboao dos Tapes", "RS", "111");
-        Pedido pedido1 = new Pedido(1, new GregorianCalendar(2022, 6, 20, 22, 30), 100.0,vendedor1);
-        Pedido pedido2 = new Pedido(2, new GregorianCalendar(2022, 4, 9, 7, 30), 70.0, vendedor2);
+        Pedido pedido1 = new Pedido(1, new GregorianCalendar(2023, 5, 21, 22, 30), 1000.0, vendedor1);
+        Pedido pedido2 = new Pedido(2, new GregorianCalendar(2023, 4, 9, 7, 30), 700.0, vendedor2);
+        Produto produto1 = new Produto(1, "Xbox One", 1, 1500.0, fornecedor1);
+        Produto produto2 = new Produto(2, "Mesa digital", 1, 500.0, fornecedor2);
+        Item item1 = new Item(1, 3.0, 1);
+        Item item2 = new Item(2, 2.0, 1);
 
     }
 }
+
+
