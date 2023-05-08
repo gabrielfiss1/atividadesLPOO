2a9d38814c2c0d646a0713762080f6e0d8d7d97c
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-05-07 22:15:31 -0300

    feat: add pedidos

--- objetivo3/objetivo3_avaliacao/src/controller/SystemController.java
+++ objetivo3/objetivo3_avaliacao/src/controller/SystemController.java
@@ -8,6 +8,7 @@ import model.Produto;
 import model.Vendedor;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class SystemController {
@@ -16,6 +17,8 @@ public class SystemController {
         Fornecedor fornecedor2 = new Fornecedor("xxxxxxx", "5399994", "Rodrigo Eletro");
         Vendedor vendedor1 = new Vendedor(1, "José", "Avenida Logo Ali", "Santa", "96323333", "Gado Grande", "RS", "011");
         Vendedor vendedor2 = new Vendedor(2, "Hugo", "Rua das Laranjas", "Cidade Baixa", "96321111", "Taboao dos Tapes", "RS", "111");
+        Pedido pedido1 = new Pedido(1, new GregorianCalendar(2022, 6, 20, 22, 30), 100.0,vendedor1);
+        Pedido pedido2 = new Pedido(2, new GregorianCalendar(2022, 4, 9, 7, 30), 70.0, vendedor2);
 
     }
 }
--- objetivo3/objetivo3_avaliacao/src/model/Pedido.java
+++ objetivo3/objetivo3_avaliacao/src/model/Pedido.java
@@ -1,12 +1,13 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class Pedido {
     private int numero;
-    private Date data;
+    private Calendar data;
     private Double valor;
 
     // ligacao entre pedido x vendedor
@@ -14,4 +15,54 @@ public class Pedido {
 
     //ligacao pedido x item
     private List<Item> items = new ArrayList();
+
+    public Pedido() {
+    }
+
+    public Pedido(int numero, Calendar data, Double valor, Vendedor vendedor) {
+        this.numero = numero;
+        this.data = data;
+        this.valor = valor;
+        this.vendedor = vendedor;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Calendar getData() {
+        return data;
+    }
+
+    public void setData(Calendar data) {
+        this.data = data;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 }
