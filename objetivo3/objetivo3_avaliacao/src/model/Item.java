a898907a618645d4fe3a7b2ac8f7561bf1025ac8
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-05-07 21:27:46 -0300

    feat: criando construtores get e setters Item e Produto

--- objetivo3/objetivo3_avaliacao/src/model/Item.java
+++ objetivo3/objetivo3_avaliacao/src/model/Item.java
@@ -7,4 +7,63 @@ public class Item {
     private int codItem;
     private double desconto;
     private int quantidade;
+    private List<Produto> produtos = new ArrayList();
+
+    public Item() {
+    }
+
+    public Item(int codItem, double desconto, int quantidade, List<Produto> produtos) {
+        this.codItem = codItem;
+        this.desconto = desconto;
+        this.quantidade = quantidade;
+        this.produtos = produtos;
+    }
+
+    public Item(int codItem, double desconto, int quantidade) {
+        this.codItem = codItem;
+        this.desconto = desconto;
+        this.quantidade = quantidade;
+    }
+
+    public int getCodItem() {
+        return codItem;
+    }
+
+    public void setCodItem(int codItem) {
+        this.codItem = codItem;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "codItem=" + codItem +
+                ", desconto=" + desconto +
+                ", quantidade=" + quantidade +
+                ", produtos=" + produtos +
+                '}';
+    }
 }
