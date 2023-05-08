adc1bf0bebd8479b8baeba960515d59d85310141
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-05-07 21:40:46 -0300

    feat: Fornecedor e SystemController

---
+++ objetivo3/objetivo3_avaliacao/src/controller/SystemController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import model.Fornecedor;
+import model.Fornecimento;
+import model.Item;
+import model.Pedido;
+import model.Produto;
+import model.Vendedor;
+public class SystemController {
+    public static void main(String[] args) {
+
+    }
+}
